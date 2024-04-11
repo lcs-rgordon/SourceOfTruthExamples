72d6a68e3172ea2cb3b18437d557c9836188be08
Russell Gordon <rgordon@lcs.on.ca>
2024-04-11 13:23:49 -0400

    Screen to generate a number.

---
+++ SourceOfTruthExamples/NumberView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  SourceOfTruthExamples
+//
+//  Created by Russell Gordon on 2024-04-11.
+//
+
+import SwiftUI
+
+struct NumberView: View {
+    
+    // MARK: Stored properties
+    @State var numbers: [Int] = []
+    @State var currentNumber: Int?
+    
+    // MARK: Computed properties
+    var body: some View {
+        VStack {
+            
+            Spacer()
+            
+            if let currentNumber = currentNumber {
+                Text("\(currentNumber)")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
+                
+                Text("You've generated this many numbers: \(numbers.count)")
+            } else {
+                Text("Generate a new number")
+                    .bold()
+                
+                Image(systemName: "arrow.down")
+            }
+            
+            Spacer()
+            
+            Button("New") {
+                currentNumber = Int.random(in: 1...100)
+                numbers.append(currentNumber!)
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NumberView()
+}
